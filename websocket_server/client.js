fdc3994fe44b4908344376780b808fcf4a7ffbe2
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2021-04-03 01:04:37 +0700

    Last commit

--- websocket_server/client.js
+++ websocket_server/client.js
@@ -1,6 +1,6 @@
 const WebSocket = require('ws');
 
-const ws = new WebSocket('ws://localhost:6001?user_id=1');
+const ws = new WebSocket('ws://baitmah.com:6001?user_id=1');
 
 ws.on('open', function open() {
 	console.log("SOCKET IS OPENED");
