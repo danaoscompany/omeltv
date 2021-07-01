25076061a45aee2631fcfd47a2038fcfe4873f9e
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2021-07-01 20:02:25 +0700

    Last commit

--- application/controllers/Util.php
+++ application/controllers/Util.php
@@ -14,8 +14,8 @@ class Util extends CI_Controller {
 	
 	public static function send_email($email, $subject, $body) {
 		$to = $email;
-		$headers = "From: " . $email . "\r\n";
-		$headers .= "Reply-To: ". $email . "\r\n";
+		$headers = "From: admin@omeltv.com\r\n";
+		$headers .= "Reply-To: admin@omeltv.com\r\n";
 		$headers .= "MIME-Version: 1.0\r\n";
 		$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 		$message = $body;
