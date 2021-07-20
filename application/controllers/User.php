4d3e9ca8a7cbe7657ac93098c38a175d0f6e5caf
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2021-07-21 06:58:23 +0700

    Last commit

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -262,10 +262,10 @@ class User extends CI_Controller {
 		}
 		if ($category == 'all' || $category == 'both') {
 			/*$partners = $this->db->query("SELECT *, SQRT(POW(69.1 * (latitude - " . $lat . "), 2) + POW(69.1 * (" . $lng . " - longitude) * COS(latitude / 57.3), 2)) AS distance FROM `users` WHERE `id` NOT IN " . $skippedIDs . " AND `id` `is_searching`=1 AND `last_searching_date` IS NOT NULL AND `last_searching_date` BETWEEN DATE_SUB('" . $date . "', INTERVAL 1 MINUTE) AND '" . $date . "' ORDER BY distance;")->result_array();*/
-			$partners = $this->db->query("SELECT *, SQRT(POW(69.1 * (latitude - " . $lat . "), 2) + POW(69.1 * (" . $lng . " - longitude) * COS(latitude / 57.3), 2)) AS distance FROM `users` WHERE `id` NOT IN " . $skippedIDs . " AND `is_searching`=1 AND `country_code`='" . $countryCode . "' ORDER BY RAND();")->result_array();
+			$partners = $this->db->query("SELECT *, SQRT(POW(69.1 * (latitude - " . $lat . "), 2) + POW(69.1 * (" . $lng . " - longitude) * COS(latitude / 57.3), 2)) AS distance FROM `users` WHERE `id` NOT IN " . $skippedIDs . " AND `is_searching`=1 AND `country_code`='" . $countryCode . "' AND `user_find_candidate`=0 ORDER BY RAND();")->result_array();
 		} else {
 			/*$partners = $this->db->query("SELECT *, SQRT(POW(69.1 * (latitude - " . $lat . "), 2) + POW(69.1 * (" . $lng . " - longitude) * COS(latitude / 57.3), 2)) AS distance FROM `users` WHERE `id` NOT IN " . $skippedIDs . " AND `gender`='" . $category . "' AND `is_searching`=1 AND `last_searching_date` IS NOT NULL AND `last_searching_date` BETWEEN DATE_SUB('" . $date . "', INTERVAL 1 MINUTE) AND '" . $date . "' ORDER BY distance;")->result_array();*/
-			$partners = $this->db->query("SELECT *, SQRT(POW(69.1 * (latitude - " . $lat . "), 2) + POW(69.1 * (" . $lng . " - longitude) * COS(latitude / 57.3), 2)) AS distance FROM `users` WHERE `id` NOT IN " . $skippedIDs . " AND `gender`='" . $category . "' AND `is_searching`=1 AND `country_code`='" . $countryCode . "' ORDER BY RAND();")->result_array();
+			$partners = $this->db->query("SELECT *, SQRT(POW(69.1 * (latitude - " . $lat . "), 2) + POW(69.1 * (" . $lng . " - longitude) * COS(latitude / 57.3), 2)) AS distance FROM `users` WHERE `id` NOT IN " . $skippedIDs . " AND `gender`='" . $category . "' AND `is_searching`=1 AND `country_code`='" . $countryCode . "' AND `user_find_candidate`=0 ORDER BY RAND();")->result_array();
 		}
 		if (sizeof($partners) <= 0) {
 			$partners = $this->db->query("SELECT * FROM `users` WHERE `id` NOT IN " . $skippedIDs . " AND `gender`='" . $category . "' AND `is_searching`=1 AND `country_code`='" . $countryCode . "'")->result_array();
@@ -1014,5 +1014,15 @@ class User extends CI_Controller {
 		} else {
 			echo 0;
 		}
+	}
+	
+	public function user_find_candidate() {
+		$userID = intval($this->input->post('user_id'));
+		$this->db->query("UPDATE `users` SET `user_find_candidate`=1 WHERE `id`=" . $userID);
+	}
+	
+	public function user_remove_candidate() {
+		$userID = intval($this->input->post('user_id'));
+		$this->db->query("UPDATE `users` SET `user_find_candidate`=0 WHERE `id`=" . $userID);
 	}
 }
