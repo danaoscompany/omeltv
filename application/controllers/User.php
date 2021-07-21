ae288f79aa5681e0bc473a4423b730608d39db55
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2021-07-21 10:56:00 +0700

    Last commit

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -273,9 +273,9 @@ class User extends CI_Controller {
 				$partners[0]['command'] = "SELECT *, SQRT(POW(69.1 * (latitude - " . $lat . "), 2) + POW(69.1 * (" . $lng . " - longitude) * COS(latitude / 57.3), 2)) AS distance FROM `users` WHERE `id` NOT IN " . $skippedIDs . " AND `gender`='" . $category . "' AND `is_searching`=1 AND `country_code`='" . $countryCode . "' AND `user_find_candidate`=0 ORDER BY RAND();";
 			}
 		}
-		if (sizeof($partners) <= 0) {
+		/*if (sizeof($partners) <= 0) {
 			$partners = $this->db->query("SELECT * FROM `users` WHERE `id`!=" . $userID . " AND `id` NOT IN " . $skippedIDs . " AND `gender`='" . $category . "' AND `is_searching`=1 AND `country_code`='" . $countryCode . "' ORDER BY RAND();")->result_array();
-		}
+		}*/
 		//echo "SELECT *, SQRT(POW(69.1 * (latitude - " . $lat . "), 2) + POW(69.1 * (" . $lng . " - longitude) * COS(latitude / 57.3), 2)) AS distance FROM `users` WHERE `id` NOT IN " . $skippedIDs . " AND `gender`='" . $category . "' AND `is_searching`=1 AND `country_code`='" . $countryCode . "' ORDER BY distance;";
 		/*if (sizeof($partners) > 0) {
 			$partners[0]['query'] = "SELECT *, SQRT(POW(69.1 * (latitude - " . $lat . "), 2) + POW(69.1 * (" . $lng . " - longitude) * COS(latitude / 57.3), 2)) AS distance FROM `users` WHERE `id` NOT IN " . $skippedIDs . " AND `gender`='" . $category . "' AND `is_searching`=1 AND `country_code`='" . $countryCode . "' ORDER BY distance;";
