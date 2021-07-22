ff6fd5d5880484c1e5b4122c5252a60e6962f983
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2021-07-22 19:42:32 +0700

    Last commit

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1043,6 +1043,12 @@ class User extends CI_Controller {
 		$this->db->query("UPDATE `users` SET `candidate_user_id`=0 WHERE `id`=" . $partnerUserID);
 	}
 	
+	public function user_remove_self_candidate() {
+		$userID = intval($this->input->post('user_id'));
+		$this->db->query("UPDATE `users` SET `user_find_candidate`=0 WHERE `id`=" . $userID);
+		$this->db->query("UPDATE `users` SET `candidate_user_id`=0 WHERE `id`=" . $userID);
+	}
+	
 	public function reset_user_connection_data() {
 		$userID = intval($this->input->post('user_id'));
 		$this->db->query("UPDATE `users` SET `is_searching`=0, `user_find_candidate`=0, `candidate_user_id`=0 WHERE `id`=" . $userID);
